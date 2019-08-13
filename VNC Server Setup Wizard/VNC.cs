b2eefbeaa8a2cf0de8279140ff1327cae9f4beec
Jack Yaz <jackyaz@users.noreply.github.com>
2019-08-13 20:51:55 +0100

    Add VNC Class and updated build options

---
+++ VNC Server Setup Wizard/VNC.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VNC_Server_Setup_Wizard
+{
+    public static class VNC_Definitions
+    {
+        public enum PlanType { Home, Professional, Enterprise }
+        public enum PermissionType { Admin, Standard, ViewOnly, None }
+
+        public enum Authentication {  VNCAuth, SystemAuth, SingleSignOn_SystemAuth }
+
+        private static PlanType plan;
+
+        public static PlanType Plan
+        {
+            get { return plan; }
+            set { plan = value; }
+        }
+    }
+}
