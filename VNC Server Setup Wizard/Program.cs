00fb003c6e7792e08e9eeadfafdb9d939a4fbef4
Jack Naisbett <jack.naisbett@gmail.com>
2021-05-12 09:46:23 +0100

    Update Program.cs
    Rename form to capital

--- VNC Server Setup Wizard/Program.cs
+++ VNC Server Setup Wizard/Program.cs
@@ -16,7 +16,7 @@ namespace VNC_Server_Setup_Wizard
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMain());
+            Application.Run(new FrmMain());
         }
     }
 }
